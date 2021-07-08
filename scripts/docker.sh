9890eb2a0e3cc7fc0548d78836174e70be3ba340
andrew-barrett <ABarrett@qa.com>
2021-07-08 17:11:09 +0100

    fixing denied request access

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo docker system prune -f
 # sudo docker rmi $(docker images -aq)
+docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
 
 cd spring-petclinic-rest-master/
 docker build -t spring-petclinic-rest-master:latest .
@@ -11,6 +12,8 @@ docker build -t spring-petclinic-angular-master:latest .
 docker tag spring-petclinic-rest-master:latest andrewbarrett18/spring-petclinic-backend:latest
 docker tag spring-petclinic-angular-master:latest andrewbarrett18/spring-petclinic-frontend:latest
 
+docker logout
+docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
 docker push andrewbarrett18/spring-petclinic-backend
 docker push andrewbarrett18/spring-petclinic-frontend
 
