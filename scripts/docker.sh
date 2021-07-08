278589c403060e57beaa29cf56d08cefebd14fcb
andrew-barrett <ABarrett@qa.com>
2021-07-08 15:27:36 +0100

    fixed docker.sh

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 sudo docker system prune
 sudo docker rmi $(docker images -aq)
-docker login
-sudo docker build -t andrewbarrett18/spring-petclinic-frontend:latest ./spring-petclinic-angular
-sudo docker build -t andrewbarrett18/spring-petclinic-backend:latest .
+docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
 
-sudo docker push andrewbarrett18/spring-petclinic-frontend:latest
-sudo docker push andrewbarrett18/spring-petclinic-backend:latest
+cd spring-petclinic-rest-master/
+docker build -t spring-petclinic-rest-master:latest .
+cd ..
+cd spring-petclinic-angular-master/
+docker build -t spring-petclinic-angular-master:latest .
+
+docker tag spring-petclinic-rest-master:latest andrewbarrett18/spring-petclinic-backend:latest
+docker tag spring-petclinic-angular-master:latest andrewbarrett18/spring-petclinic-frontend:latest
+
+docker push andrewbarrett18/spring-petclinic-backend
+docker push andrewbarrett18/spring-petclinic-frontend
 
 source ~/.bashrc
