32782d83817ea7b80dcd895c08d444942d77fec4
andrew-barrett <ABarrett@qa.com>
2021-07-08 17:46:30 +0100

    aws configure

--- scripts/clusterconfig.sh
+++ scripts/clusterconfig.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+aws configure set aws_access_key_id $ACCESS_KEY
+aws configure set aws_secret_access_key $SECRET_ACCESS_KEY
+
 cd kubernetes
 eksctl create cluster -f eks-pcp.yaml
 cd
