b5f8454dbabc0dffbd59fd97faa2ce653d9463a5
andrew-barrett <ABarrett@qa.com>
2021-07-08 17:24:28 +0100

    installation of terraform

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -30,3 +30,10 @@ sudo mv /tmp/eksctl /usr/local/bin
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 sudo chmod +x kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl 
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y unzip wget
+wget https://releases.hashicorp.com/terraform/0.14.6/terraform_0.14.6_linux_amd64.zip
+unzip terraform_*_linux_*.zip
+sudo mv terraform /usr/local/bin
+rm terraform_*_linux_*.zip
